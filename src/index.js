de23ebc02c744c8f1ba2c1e930676d54a25196d1
Serhii Naboka <drifterzzz@gmail.com>
2023-03-26 15:32:49 +0300

    in process

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const inputRef = document.querySelector('#search-box');
+const countriesListRef = document.querySelector('.country-list');
+const countriesInfoRef = document.querySelector('.country-info');
+
+inputRef.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange() {
+  const name = inputRef.value.trim();
+
+  if (name === '') {
+    return countriesInfoRef.innerHTML === '', countriesListRef.innerHTML === '';
+  }
+
+  fetchCountries(name)
+    .then(countries => {
+      countriesListRef.innerHTML === '', countriesInfoRef.innerHTML === '';
+      if (countries.length === 1) {
+        console.log(countries);
+      } else if (countries.length >= 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else {
+        console.log(countries);
+      }
+    })
+    .catch(Notiflix.Notify.failure('Oops, there is no country with that name'));
+
+}
